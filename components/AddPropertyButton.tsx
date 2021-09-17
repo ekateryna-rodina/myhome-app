9ed45af4f749299c1d6c8adf29d28e74fb147815
ekateryna-rodina <work.rodina@gmail.com>
2021-09-17 11:08:43 -0400

    add property button

--- components/AddPropertyButton.tsx
+++ components/AddPropertyButton.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "./Button.style";
+
+const Label = styled.span<{ margin?: string }>`
+  text-transform: capitalize;
+  margin: ${(props) => (props.margin ? props.margin : "inherit")};
+`;
 
 const AddPropertyButton = () => {
-  return <div></div>;
+  return (
+    <Button background={"secondary"} size={"large"}>
+      <Label margin={"0 .5rem 0 0"}>+</Label>
+      <Label>add property</Label>
+    </Button>
+  );
 };
 
 export default AddPropertyButton;
---
+++ components/Button.style.tsx
@@ -0,0 +1,40 @@
+import { lighten } from "polished";
+import React from "react";
+import styled from "styled-components";
+const padding: { [key: string]: string } = {
+  large: "1rem 1.25rem",
+};
+const StyledButton = styled.button<ButtonProps>`
+  padding: ${(props) => padding[props.size]};
+  background: ${(props) => props.theme[props.background]};
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: none;
+  border-radius: 0.2rem;
+  color: white;
+  transition: all 0.5s;
+  font-size: 1rem;
+  :hover,
+  :focus {
+    background: ${(props) => lighten(0.1, props.theme.secondary)};
+    border: none;
+    outline: 0;
+  }
+`;
+type ButtonProps = {
+  size: "large" | "medium" | "small";
+  background: string;
+};
+const Button: React.FC<ButtonProps> = ({ children, size, background }) => {
+  return (
+    <StyledButton size={size} background={background}>
+      {children}
+    </StyledButton>
+  );
+};
+
+export default Button;
