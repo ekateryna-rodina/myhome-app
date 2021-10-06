302f8512b916f96b2081c4616cb160e917b9d271
ekateryna-rodina <work.rodina@gmail.com>
2021-10-06 09:12:21 -0400

    global as any

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -28,11 +28,11 @@ if (typeof window === "undefined") {
   if (process.env.NODE_ENV === "production") {
     prisma = new PrismaClient();
   } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
+    if (!(global as any).prisma) {
+      (global as any).prisma = new PrismaClient();
     }
 
-    prisma = global.prisma;
+    prisma = (global as any).prisma;
   }
 }
 
